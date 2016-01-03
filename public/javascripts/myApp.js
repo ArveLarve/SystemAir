a11b571f205ed240c507a42e6a99492d1f0eeea7
Arve <arvelotveit@MacMini-2.local>
2016-01-03 15:47:55 +0100

    Initial commit

---
+++ public/javascripts/myApp.js
@@ -0,0 +1,18 @@
+
+var socket = io();
+$(document).ready(function(){
+	$('input[type=radio][name=speedMode]').change(function(){
+		socket.emit('speedMode', this.value);
+	})
+	$('#unBalanced').change(function(){
+		socket.emit('unBalanced', $(this).is(':checked'));
+	})
+});
+socket.on('speedMode', function(value){
+    $('input[type=radio][name=speedMode][value=' + value + ']').prop('checked', true);
+    console.log("speedMode: " + value);
+  });
+socket.on('unBalanced', function(value){
+    $('#unBalanced').prop('checked', value);
+    console.log("unBalanced: " + value);
+  });
